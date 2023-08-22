1cbd17a3958f6123bc999b4bb8219f5bb73c6edf
Ubaydulloh Kuchkarov <ubaydulloh1000@gmail.com>
2023-08-22 19:28:48 +0500

    Update settings.py

--- goodreads/settings.py
+++ goodreads/settings.py
@@ -155,8 +155,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ubaydulloh1000@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ub@ydull04Email'
+EMAIL_HOST_USER = 'dummy@gmail.com'
+EMAIL_HOST_PASSWORD = 'dummy'
 
 
 # Rest Framework configurations
